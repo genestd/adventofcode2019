bbeb79e0dbcf31c06bc67e4842e91598a49fc388
Dave Genest <dgenest@davids-mbp.lan>
2019-12-03 21:04:52 -0800

    AoC day 1 solution

--- day1/fuelReq.js
+++ day1/fuelReq.js
@@ -3,8 +3,14 @@ const data = require('./input')
 const calc = value => Math.floor(value/3) -2
 
 const main = () => {
-    const total = data.reduce((result, input) => { return result + calc(input) }, 0)
+    const total = data.reduce((result, input) => { 
+        let fuelMass = calc(input)
+        while (fuelMass > 0) {
+            result = result + fuelMass
+            fuelMass = calc(fuelMass)
+        }
+        return result
+    }, 0)
     console.log(total)
 }
-
 main()
